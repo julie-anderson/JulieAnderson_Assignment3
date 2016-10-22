5cd0048df3276a22f07eb77cbbbded7f5862e195
Julie Anderson <juliemanderson@gmail.com>
2016-10-22 11:12:40 -0500

    Remove id bug in wines.js

--- routes/wines.js
+++ routes/wines.js
@@ -20,9 +20,10 @@ db.open(function(err, db) {
 });
 
 exports.addRecord = function(req, res) {
-  console.log('Inserting restaurant: ' + id);
+  console.log('Inserting restaurant: ');
   db.collection('restaurants', function(err, collection) {
-    collection.insertOne({"_id":"980b6b5414bedce366a46c01","address":{"building":"469","coord":[-73.961704,40.662942],"street":"Flatbush Avenue","zipcode":"11225"},"borough":"Brooklyn","cuisine":"Hamburgers","grades":[{"date":"2014-12-30T00:00:00.000Z","grade":"A","score":8},{"date":"2014-07-01T00:00:00.000Z","grade":"B","score":23},{"date":"2013-04-30T00:00:00.000Z","grade":"A","score":12},{"date":"2012-05-08T00:00:00.000Z","grade":"A","score":12}],"name":"Wendy'S","restaurant_id":"30112340"}, function(err, item) {
+    collection.insertOne({"_id":"980b6b5414bedce366a46c01","address":{"building":"469","coord":[-73.961704,40.662942],"street":"Flatbush Avenue","zipcode":"11225"},"borough":"Brooklyn","cuisine":"Hamburgers","grades":[{"date":"2014-12-30T00:00:00.000Z","grade":"A","score":8},{"date":"2014-07-01T00:00:00.000Z","grade":"B","score":23},{"date":"2013-04-30T00:00:00.000Z","grade":"A","score":12},{"date":"2012-05-08T00:00:00.000Z","grade":"A","score":12}],"name":"Wendy'S","restaurant_id":"30112340"},
+        function(err, item) {
       res.send(item);
     });
   });
